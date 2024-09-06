5742cc9f89c8c6a2f44426499520dd18da2a7530
Firman-001 <pirman475@smk.belajar.id>
2024-09-06 09:58:38 +0700

    Scroll Up

--- main.js
+++ main.js
@@ -15,7 +15,19 @@ navLink.forEach(link => {
   });
 });
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+const scrollUp = () => {
+  const scrollUpBtn = document.getElementById('scroll-up');
 
+  if (this.scrollY  >= 250) {
+    scrollUpBtn.classList.remove('-bottom-1/2');
+    scrollUpBtn.classList.add('bottom-4'); 
+  } else {
+    scrollUpBtn.classList.remove('bottom-4');
+    scrollUpBtn.classList.add('-bottom-1/2');
+  }
+};
+
+window.addEventListener('scroll', scrollUp);
 
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
 
